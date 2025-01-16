85a16d4a1642d4b25d9bc9dd39a5725cdc742d73
joy628 <joy_sun628@outlook.com>
2025-01-16 12:31:51 +0100

    further process timeseries data- calculate low maks ratio

---
+++ dataloader/convert.py
@@ -0,0 +1,65 @@
+import numpy as np
+import pandas as pd
+from pathlib import Path
+
+from utils import write_json, write_pkl, load_json
+
+def convert_timeseries_to_mmap(data_dir, save_dir, n_rows=100000):
+    """Convert timeseries CSV files to memory-mapped files."""
+    save_path = Path(save_dir) / 'ts.dat'
+    shape = (n_rows, 24, 34)
+    mmap_file = np.memmap(save_path, dtype=np.float32, mode='w+', shape=shape)
+    ids, total_rows = [], 0
+    info = {'name': 'ts', 'shape': shape}
+
+    for split in ['train', 'val', 'test']:
+        print(f'Processing {split} split...')
+        csv_path = Path(data_dir) / split / 'timeseries.csv'
+        df = pd.read_csv(csv_path).values.reshape(-1, 24, 35)
+        ids.append(df[:, 0, 0])
+        mmap_file[total_rows:total_rows + len(df), :, :] = df[:, :, 1:]
+        info[f'{split}_len'] = len(df)
+        total_rows += len(df)
+
+    info['total'] = total_rows
+    info['columns'] = list(pd.read_csv(csv_path).columns[1:])
+    ids = np.concatenate(ids)
+    id2pos = {pid: idx for idx, pid in enumerate(ids)}
+    pos2id = {idx: pid for idx, pid in enumerate(ids)}
+
+    print('Saving metadata...')
+    write_pkl(id2pos, Path(save_dir) / 'id2pos.pkl')
+    write_pkl(pos2id, Path(save_dir) / 'pos2id.pkl')
+    write_json(info, Path(save_dir) / 'ts_info.json')
+    print('Done.')
+
+def convert_csv_to_mmap(data_dir, save_dir, csv_name, n_cols=None, n_rows=100000):
+    """Convert flat CSV files to memory-mapped files."""
+    column_map = {'diagnoses': 520, 'diagnoses_1033': 1034, 'labels': 5, 'flat': 58}
+    n_cols = (column_map[csv_name] - 1) if n_cols is None else n_cols
+    shape = (n_rows, n_cols)
+
+    save_path = Path(save_dir) / f'{csv_name}.dat'
+    mmap_file = np.memmap(save_path, dtype=np.float32, mode='w+', shape=shape)
+    info = {'name': csv_name, 'shape': shape}
+
+    total_rows = 0
+    for split in ['train', 'val', 'test']:
+        print(f'Processing {split} split for {csv_name}...')
+        csv_path = Path(data_dir) / split / f'{csv_name}.csv'
+        df = pd.read_csv(csv_path).iloc[:, 1:].values  # Exclude the patient column
+        mmap_file[total_rows:total_rows + len(df), :] = df
+        info[f'{split}_len'] = len(df)
+        total_rows += len(df)
+
+    info['total'] = total_rows
+    info['columns'] = list(pd.read_csv(csv_path).columns[1:])
+    write_json(info, Path(save_dir) / f'{csv_name}_info.json')
+    print('Done.')
+
+
+def read_mm(data_dir, name):
+    """Load memory-mapped data and its metadata."""
+    info = load_json(Path(data_dir) / f'{name}_info.json')
+    data = np.memmap(Path(data_dir) / f'{name}.dat', dtype=np.float32, shape=tuple(info['shape']))
+    return data, info

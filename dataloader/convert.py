ed85b24659779be0b6d44ca63cc5bf22350c7506
joy628 <joy_sun628@outlook.com>
2025-01-19 14:13:09 +0100

    convert csv to mmap

--- dataloader/convert.py
+++ dataloader/convert.py
@@ -4,10 +4,16 @@ from pathlib import Path
 
 from utils import write_json, write_pkl, load_json
 
-def convert_timeseries_to_mmap(data_dir, save_dir, n_rows=100000):
+def convert_timeseries_to_mmap(data_dir, save_dir):
     """Convert timeseries CSV files to memory-mapped files."""
+    total_rows = 0
+    for split in ['train', 'val', 'test']:
+        csv_path = Path(data_dir) / split / 'timeseries.csv'
+        total_rows += sum(1 for _ in open(csv_path)) - 1  # 减去标题行
+
+    n_rows = total_rows
     save_path = Path(save_dir) / 'ts.dat'
-    shape = (n_rows, 24, 115)
+    shape = (n_rows, 290, 115)
     mmap_file = np.memmap(save_path, dtype=np.float32, mode='w+', shape=shape)
     ids, total_rows = [], 0
     info = {'name': 'ts', 'shape': shape}
@@ -15,7 +21,7 @@ def convert_timeseries_to_mmap(data_dir, save_dir, n_rows=100000):
     for split in ['train', 'val', 'test']:
         print(f'Processing {split} split...')
         csv_path = Path(data_dir) / split / 'timeseries.csv'
-        df = pd.read_csv(csv_path).values.reshape(-1, 24,116)
+        df = pd.read_csv(csv_path).values.reshape(-1, 290, 116)
         ids.append(df[:, 0, 0])
         mmap_file[total_rows:total_rows + len(df), :, :] = df[:, :, 1:]
         info[f'{split}_len'] = len(df)
@@ -33,10 +39,15 @@ def convert_timeseries_to_mmap(data_dir, save_dir, n_rows=100000):
     write_json(info, Path(save_dir) / 'ts_info.json')
     print(f"Timeseries conversion complete. Info: {info}")
 
-def convert_csv_to_mmap(data_dir, save_dir, csv_name, n_cols=None, n_rows=100000):
+def convert_csv_to_mmap(data_dir, save_dir, csv_name, n_cols=None):
     """Convert flat CSV files to memory-mapped files."""
-    column_map = {'diagnoses': 116,  'labels': 4, 'flat': 107}
+    column_map = {'diagnoses': 118,  'labels': 5, 'flat': 105}
     n_cols = (column_map[csv_name] - 1) if n_cols is None else n_cols
+    total_rows = 0
+    for split in ['train', 'val', 'test']:
+        csv_path = Path(data_dir) / split / f'{csv_name}.csv'
+        total_rows += sum(1 for _ in open(csv_path)) - 1  # 减去标题行
+    n_rows = total_rows
     shape = (n_rows, n_cols)
 
     save_path = Path(save_dir) / f'{csv_name}.dat'
